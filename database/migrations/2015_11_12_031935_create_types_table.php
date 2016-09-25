b1a52d29237e136565cead3c68383da1bc57a8d4
André Amorim <adz.amorim@gmail.com>
2016-09-25 17:08:16 -0300

    Adjusting migrations and .gitignore

--- database/migrations/2015_11_12_031935_create_types_table.php
+++ database/migrations/2015_11_12_031935_create_types_table.php
@@ -16,12 +16,7 @@ class CreateTypesTable extends Migration {
             $table->engine = 'InnoDB';
             $table->bigIncrements('id');
             $table->string('type');
-            $table->bigInteger('user_id')->unsigned();
             $table->timestamps();
-        });
-
-        Schema::table('types', function($table){
-			$table->foreign('user_id')->references('id')->on('users');
         });
 	}
 
--- database/migrations/2015_11_12_042759_create_transactions_table.php
+++ database/migrations/2015_11_12_042759_create_transactions_table.php
@@ -39,7 +39,69 @@ class CreateTransactionsTable extends Migration {
             $table->foreign('vendor_id')->references('id')->on('vendors');
             $table->foreign('user_id')->references('id')->on('users');
         });
-      }
+
+        $sql = "
+            CREATE PROCEDURE `spSetAccountAmount`(IN curr_account INT)
+            BEGIN
+              DECLARE curr_balance DECIMAL(20,2);
+                
+              SELECT 
+                SUM(CASE WHEN type_id = 3 AND account_id = curr_account THEN amount * -1 ELSE amount END) AS TOTAL
+                INTO curr_balance
+              FROM transactions
+                WHERE account_id = curr_account
+              OR accountto_id = curr_account
+              ORDER BY date;
+                
+                UPDATE accounts 
+                SET balance = curr_balance
+                WHERE id = curr_account;
+            END
+        ";
+
+        DB::unprepared($sql);
+
+        $sql = "
+            CREATE PROCEDURE `spTotalCategory`(IN due_month INT, IN due_year INT, IN type_id INT)
+            BEGIN
+                SELECT 
+                    `c`.`id`,
+                    `c`.`category` AS `category`,
+                    SUM(`t`.`amount`) AS `total_amount`
+                FROM
+                    (`transactions` `t`
+                    LEFT JOIN `categories` `c` ON ((`t`.`category_id` = `c`.`id`)))
+                WHERE `t`.`due_month` = due_month
+                AND `t`.`due_year` = due_year             
+                AND `t`.`type_id` = type_id
+                GROUP BY `t`.`category_id`
+                ORDER BY `total_amount`;
+            END
+        ";
+
+        DB::unprepared($sql);
+
+        $sql = "
+            CREATE PROCEDURE `spTotalSubCategory`(IN category_id INT, IN due_month INT, IN due_year INT, IN type_id INT)
+            BEGIN
+                SELECT 
+                    `s`.`id`,
+                    `s`.`subcategory` AS `subcategory`,
+                    SUM(`t`.`amount`) AS `total_amount`
+                FROM
+                    (`transactions` `t`
+                    LEFT JOIN `subcategories` `s` ON ((`t`.`subcategory_id` = `s`.`id`)))
+                WHERE `t`.`due_month` = due_month
+                AND `t`.`due_year` = due_year             
+                AND `t`.`type_id` = type_id
+                AND `t`.`category_id` = category_id
+                GROUP BY `t`.`subcategory_id`
+                ORDER BY `total_amount`;
+            END
+        ";
+
+        DB::unprepared($sql);
+    }
 
       /**
        * Reverse the migrations.
@@ -49,5 +111,8 @@ class CreateTransactionsTable extends Migration {
       public function down()
       {
       	Schema::drop('transactions');
+        DB::unprepared("DROP procedure IF EXISTS spSetAccountAmount");
+        DB::unprepared("DROP procedure IF EXISTS spTotalCategory");
+        DB::unprepared("DROP procedure IF EXISTS spTotalSubCategory");
       }
 }
